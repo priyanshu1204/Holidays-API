d06f4083e26ffa80287e164d3f2fb14a1272056f
Priyanshu Raj <115916385+priyanshu1204@users.noreply.github.com>
2025-06-27 18:19:01 +0530

    update index for ci integration

--- index.js
+++ index.js
@@ -4,10 +4,14 @@ const app = require('./app');
 
 const PORT = process.env.PORT || 3000;
 
+console.log("MONGODB_URI:", process.env.MONGODB_URI);
+
 mongoose.connect(process.env.MONGODB_URI)
   .then(() => {
     console.log('Connected to MongoDB');
-   app.listen(PORT, '0.0.0.0', () => console.log(`API running on port ${PORT}`));
-
+    app.listen(PORT, '0.0.0.0', () => console.log(`API running on port ${PORT}`));
   })
-  .catch(err => console.error('MongoDB connection error:', err));
+  .catch(err => {
+    console.error('MongoDB connection error:', err);
+    process.exit(1); // Fail fast if DB is not reachable
+  });
