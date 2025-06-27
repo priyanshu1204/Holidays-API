70e8d0a4ead3b59abdf0db3e721c7222916ae1eb
Priyanshu Raj <115916385+priyanshu1204@users.noreply.github.com>
2025-06-27 17:57:50 +0530

    updated ci

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const PORT = process.env.PORT || 3000;
 mongoose.connect(process.env.MONGODB_URI)
   .then(() => {
     console.log('Connected to MongoDB');
-    app.listen(PORT, () => console.log(`API running on port ${PORT}`));
+   app.listen(PORT, '0.0.0.0', () => console.log(`API running on port ${PORT}`));
+
   })
   .catch(err => console.error('MongoDB connection error:', err));
